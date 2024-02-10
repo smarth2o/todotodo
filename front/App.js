7acbef25bf98d449a2ddefe6221f49f00511b0d2
김현정 <khjung11203@gmail.com>
2024-02-10 18:47:36 +0900

    add color font header

--- front/App.js
+++ front/App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { theme } from "./colors";
+import { useFonts } from "expo-font";
+import { useState } from "react";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "NanumSquareNeoOTF-Bd": require("./assets/fonts/NanumSquareNeoOTF-Bd.otf"),
+    "NanumSquareNeoOTF-Eb": require("./assets/fonts/NanumSquareNeoOTF-Eb.otf"),
+    "NanumSquareNeoOTF-Hv": require("./assets/fonts/NanumSquareNeoOTF-Hv.otf"),
+  });
+
+  const [title, setTitle] = useState("todo");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Pressable onPress={() => setTitle("todo")}>
+          <Text
+            style={{
+              ...styles.title,
+              color:
+                title === "todo" ? theme.titleSelected : theme.titleUnselected,
+            }}
+          >
+            투두
+          </Text>
+        </Pressable>
+        <Pressable onPress={() => setTitle("category")}>
+          <Text
+            style={{
+              ...styles.title,
+              color:
+                title === "category"
+                  ? theme.titleSelected
+                  : theme.titleUnselected,
+            }}
+          >
+            카테고리
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,8 +49,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.background,
+    paddingHorizontal: 20,
+  },
+  header: {
+    marginTop: 100,
+    flexDirection: "row",
+  },
+  title: {
+    fontSize: 38,
+    fontFamily: "NanumSquareNeoOTF-Hv",
+    paddingHorizontal: 10,
   },
 });
